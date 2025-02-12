d036c59b89c25a2754d9e33f6b5aa7c19160f3a0
Shelly Lachish <shellstar@gmail.com>
2025-02-13 06:35:41 +1000

    puts taglist outside of page_navbar() to fix warning message

--- app.R
+++ app.R
@@ -11,61 +11,62 @@ source('./modules/heatmap_module.R')
 source('./modules/stats_module.R')
 
 
-ui <- page_navbar(
+ui <- tagList(
   tags$head(
     tags$style(HTML("
-    .shiny-notification {
-      position: fixed !important;
-      top: 400px !important;
-      left: 500px !important;
-      z-index: 1050;
-    }
-  "))
+      .shiny-notification {
+        position: fixed !important;
+        top: 400px !important;
+        left: 500px !important;
+        z-index: 1050;
+      }
+    "))
   ),
 
-  title = "Wild Toohey Explorer",
+  page_navbar(
+    title = "Wild Toohey Explorer",
 
-  sidebar = sidebar(
-    speciesSelectionUI("species")
-  ),
+    sidebar = sidebar(
+      speciesSelectionUI("species")
+    ),
 
-  nav_panel(
-    title = "Fauna Finder",
-    mapModuleUI("finder")
-  ),
+    nav_panel(
+      title = "Fauna Finder",
+      mapModuleUI("finder")
+    ),
 
-  nav_panel(
-    title = "Hotspots",
-    heatmapModuleUI("heatmap")
-  ),
+    nav_panel(
+      title = "Hotspots",
+      heatmapModuleUI("heatmap")
+    ),
 
-  nav_panel(
-    title = "Trends",
-    statsModuleUI("stats")
-  ),
+    nav_panel(
+      title = "Trends",
+      statsModuleUI("stats")
+    ),
 
-  nav_panel(
-    title = "Species List ",
-    card(
-      "Species list table with images to go here"
-    )
-  ),
+    nav_panel(
+      title = "Species List ",
+      card(
+        "Species list table with images to go here"
+      )
+    ),
 
-  nav_panel(
-    title = "Fact File",
-    card(
-      "Resource links and information will go here"
-    )
-  ),
+    nav_panel(
+      title = "Fact File",
+      card(
+        "Resource links and information will go here"
+      )
+    ),
 
-  nav_panel(
-    title = "About",
-    card(
-      "Resource links and information will go here"
+    nav_panel(
+      title = "About",
+      card(
+        "Resource links and information will go here"
+      )
     )
   )
 )
-
 
 server <- function(input, output, session) {
   # Get filtered data from species selection module
