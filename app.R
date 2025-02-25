803c617ced210de42ec2c65f073f1fe3cefa48ea
Shelly Lachish <shellstar@gmail.com>
2025-02-25 21:11:40 +1000

    hides sidebar on last three navpanel pages

--- app.R
+++ app.R
@@ -1,8 +1,6 @@
 options(shiny.autoreload = TRUE)
-
 # Source functions
 source('./modules/functions.R')
-
 # Source modules
 source('./modules/global.R')
 source('./modules/species_selector_module.R')
@@ -10,7 +8,6 @@ source('./modules/map_module.R')
 source('./modules/heatmap_module.R')
 source('./modules/stats_module.R')
 source('./modules/specieslist_module.R')
-
 
 ui <- tagList(
   tags$head(
@@ -21,44 +18,38 @@ ui <- tagList(
         left: 500px !important;
         z-index: 1050;
       }
-
     "))
   ),
 
   page_navbar(
     title = "Wild Toohey Explorer",
-
+    id = "navbarpage",
     sidebar = sidebar(
+      id = 'main_sidebar',
       speciesSelectionUI("species")
     ),
-
     nav_panel(
       title = "Fauna Finder",
       mapModuleUI("finder")
     ),
-
     nav_panel(
       title = "Trends",
       statsModuleUI("stats")
     ),
-
     nav_panel(
       title = "Hotspots",
       heatmapModuleUI("heatmap")
     ),
-
-   nav_panel(
+    nav_panel(
       title = "Species List",
       specieslistModuleUI("specieslist")
     ),
-
     nav_panel(
       title = "Fact File",
       card(
         "Resource links and information will go here"
       )
     ),
-
     nav_panel(
       title = "About",
       card(
@@ -69,7 +60,6 @@ ui <- tagList(
 )
 
 server <- function(input, output, session) {
-
   # Get filtered data from species selection module
   species_data <- speciesSelectionServer("species")
 
@@ -79,10 +69,27 @@ server <- function(input, output, session) {
   statsModuleServer("stats",
                     filtered_data = species_data$filtered_data,
                     taxa_level = species_data$taxa_level)
+
   # Use species list table to get table
   specieslistModuleServer("specieslist",
                           species_list)
 
+  # Observe navbar tab changes to toggle sidebar
+  observe({
+    # Define the panels that should have the sidebar closed
+    hide_sidebar_on <- c("Species List", "Fact File", "About")
+    current_tab <- input$navbarpage
+
+    # Check if the current tab should have the sidebar hidden
+    if (current_tab %in% hide_sidebar_on) {
+      # Close the sidebar
+      bslib::toggle_sidebar(id = "main_sidebar", open = FALSE)
+    } else {
+      # Open the sidebar
+      bslib::toggle_sidebar(id = "main_sidebar", open = TRUE)
+    }
+  })
 }
+
 
 shinyApp(ui, server)
