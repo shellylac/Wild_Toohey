312047c39b2b2e060da22fde67b6ef49fffb773b
Shelly Lachish <shellstar@gmail.com>
2025-04-13 09:19:15 +1000

    reduces words

--- modules/about_module.R
+++ modules/about_module.R
@@ -4,20 +4,22 @@
 aboutModuleUI <- function(id) {
   ns <- NS(id)
 
-
-
   div(
+    div(
+      style = "text-align: center; margin-bottom: 20px;",
+      h2(em("Toohey Forest holds a special place in my heart!"), style = "font-size: 1.2em;"),
+    ),
 
     card(
+      card_header(
+        bsicons::bs_icon("star-fill", size = "1.5rem"),
+        span("Inspiration", class = "ms-2")
+      ),
       card_body(
-        div(
-          style = "text-align: center; margin-bottom: 20px;",
-          h2("Toohey Forest holds a special place in my heart!", style = "font-size: 1.2em;"),
-          p("As a Tarragindi local, I grew up with the forest right in my backyard. I've spent countless hours walking, running, cycling, birdwatching, spotlighting, geocaching, and picnicking here. I adore its striking beauty and feel especially privileged by the wildlife encounters I've had—although I know firsthand how surprisingly challenging it can be to spot these amazing creatures!"),
+          p("As a Tarragindi local, I grew up with the forest right in my backyard, and have spent countless hours exploring and enjoying this unique bushland island. I adore its quiet natural beauty and feel especially privileged by the wildlife encounters I've had — although I know firsthand how surprisingly challenging it can be to spot these amazing creatures!"),
           p("Wild Toohey was inspired by my desire to make wildlife spotting easier and more rewarding, not just for myself but for everyone who loves Toohey Forest."),
-          p("With Wild Toohey, you have instant access to everything you need for memorable wildlife experiences: interactive maps of recent and historic sightings, detailed charts showing annual, monthly, and daily wildlife trends, valuable insights into seasonal wildlife hotspots, and a comprehensive species list complete with images and helpful information.")
+          p("With Wild Toohey, you have instant access to everything you need for memorable wildlife experiences: interactive maps of recent and historic sightings, detailed charts showing annual, monthly, and daily wildlife trends, maps of seasonal wildlife hotspots, a species list complete with images, taxonomy, and links to wikipedia.")
           )
-        )
       ),
 
     # Feature cards section
@@ -26,6 +28,7 @@ aboutModuleUI <- function(id) {
       layout_column_wrap(
         width = "250px",
         fill = FALSE,
+
         # DATA CARD ----
         card(
           card_header(
@@ -39,7 +42,7 @@ aboutModuleUI <- function(id) {
               ),
             p("Wild Toohey accesses ALA databases to extract records of species occurrence within the broad bounds of Toohey Forest over the past 10 years. The data were filtered using the ",
               a(href = "https://support.ala.org.au/support/solutions/articles/6000240256-getting-started-with-the-data-profiles", "ALA data profile"),
-              ", which applies a standard set of data quality filters. The data behind Wild Toohey updates daily every Thursday, Friday, Saturday and Sunday."
+              ", which applies a standard set of data quality filters. The data behind Wild Toohey updates daily at half past midnight every Thursday, Friday, Saturday and Sunday."
               )
             )
           ),
@@ -62,8 +65,8 @@ aboutModuleUI <- function(id) {
         card(
           card_header(
             bsicons::bs_icon("hand-thumbs-up-fill", size = "1.5rem"),
-            span("Acknowledgments", class = "ms-2")
-            ),
+            span("Acknowledgements", class = "ms-2")
+          ),
           card_body(
             p("Wild Toohey gratefully acknowledges and pays respect to the traditional owners of the land on which Toohey Forest stands, including the Turrbal and Yuggera peoples, their culture, and their Elders past, present, and emerging. We respect their continuing culture and the contribution they make to life in Brisbane."),
             p("Wild Toohey would like to thank all the citizen scientists, researchers, and organizations who have recorded wildlife sightings and contributed data to make this project possible.")
@@ -71,7 +74,6 @@ aboutModuleUI <- function(id) {
           )
         )
       )
-
     )
 }
 
