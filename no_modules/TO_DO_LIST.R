7804c4614d8993aa043597b7a686b451deb9a1a3
Shelly Lachish <shellstar@gmail.com>
2025-04-23 14:03:12 +1000

    updates to do list

--- no_modules/TO_DO_LIST.R
+++ no_modules/TO_DO_LIST.R
@@ -1,11 +1,9 @@
 # These are my TO-DOS
-# * Add value boxes - most likely to see and least likely seen
-# Use shinyrouter(?) to add urls for subpages
-# Set up new shinyapps account wildapps/shiny....
-# # class = "rounded-accordion" on species list section
-# ----- # Rounded class here prevents common name list from scrolling
 #> * No data warning message showing up before heatmap is generated.
+#>
 #> * Prettify the app! CSS Styling and colours and images!!
+#>  --- remove all the UI styling to a css file
+#>  --- add a bespoke bslib theme
 #>
 #> ADD RENV for package management
 #> ADD testing!?
