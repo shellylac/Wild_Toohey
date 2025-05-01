9be20f1f841d7f28f8d59a1633b750ddd4f526af
Shelly Lachish <shellstar@gmail.com>
2025-05-02 09:57:05 +1000

    shifts data preprocessing to ALA repo

--- modules/global.R
+++ modules/global.R
@@ -34,30 +34,7 @@ DEFAULT_ZOOM <- 13
 
 # Read in the occurrence data ----
 data_url <- "https://raw.githubusercontent.com/shellylac/ALA_Toohey_Data/main/output_data/toohey_species_occurrences.rds"
-toohey_occs <- readRDS(url(data_url)) |>
-  # Just a catch in case an NA species gets through
-  dplyr::filter(!is.na(species)) |>
-  # Add common class names
-  dplyr::mutate(class_common = case_match(class,
-                                          "Aves" ~ "Birds",
-                                          "Mammalia" ~ "Mammals",
-                                          "Reptilia" ~ "Reptiles",
-                                          "Amphibia" ~ "Amphibians"),
-                class_common = factor(class_common,
-                                      levels = c("Birds", "Mammals",
-                                                 "Reptiles", "Amphibians")
-                ),
-                # Add colour for trend plots (based on class_common)
-                plot_colour = case_match(class_common,
-                                         "Birds" ~ STATS_BLUE,
-                                         "Mammals" ~ STATS_RED,
-                                         "Reptiles" ~ STATS_ORANGE,
-                                         "Amphibians" ~ STATS_GREEN),
-                # Add formatted dates for stats plots
-                year = as.factor(lubridate::year(eventDate)),
-                month = lubridate::month(eventDate, label = TRUE),
-                hour = as.factor(lubridate::hour(hms(eventTime)))
-                )
+toohey_occs <- readRDS(url(data_url))
 
 
 # Read in the toohey shapefile ----
@@ -65,29 +42,7 @@ boundary_url <- "./data/toohey_shapefiles/toohey_forest_boundary.shp"
 toohey_outline <- sf::st_read(boundary_url)
 
 
-# Generate the species list table ----
-species_list <- toohey_occs |>
-  dplyr::group_by(class_common, class, order, family, species, vernacular_name,
-                  wikipedia_url, image_url) |>
-  count(name = "Sightings") |>
-  ungroup() |>
-  rename(Class = class,  `Common name` = vernacular_name) |>
-  mutate(
-    Taxonomy = paste0("<p style=\"font-size:14px;\">",
-                      "<a href=\"", wikipedia_url, "\" target=\"_blank\">",
-                      `Common name`, "</a>", "<br>",
-                      "<b>Class</b>: ", Class, "<br>",
-                      "<b>Order</b>: ", order, "<br>",
-                      "<b>Family</b>: ", family, "<br>",
-                      "<b>Species</b>: <em>", species,
-                      "</em></p>"),
-    Image = paste0("<img src=\"", image_url,
-                   "\" height=\"120\" data-toggle=\"tooltip\" data-placement=\"center\" title=\"",
-                   `Common name`, "\"></img>", "</p>")
-  ) |>
-  arrange(desc(Sightings),
-          factor(Class, levels = c('Aves', 'Mammalia', 'Reptilia', 'Amphibia')),
-          `Common name`) |>
-  select(Class, Taxonomy, Image, Sightings)
-
+# Read in the species list data set ----
+species_list_url <- "https://raw.githubusercontent.com/shellylac/ALA_Toohey_Data/main/output_data/toohey_species_list.rds"
+species_list <- readRDS(url(species_list_url))
 
