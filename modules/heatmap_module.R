06578174987525f9cb879646b6c8fcae0b59312d
Shelly L <shellstar@gmail.com>
2026-09-11 12:20:44 +1000

    adds updated base layer to heatmap too

--- modules/heatmap_module.R
+++ modules/heatmap_module.R
@@ -16,7 +16,7 @@ heatmapModuleUI <- function(id) {
     div(
       id = ns("heatmap_legend_container"),
       style = "margin-bottom: 10px; text-align: center;"
-     )
+    )
   )
 }
 
@@ -29,19 +29,33 @@ heatmapModuleServer <- function(id, filtered_data, taxa_level) {
     # Create a debounced version of filtered_data
     debounced_data <- reactive({
       filtered_data()
-    }) |> debounce(1000)  # 1000ms debounce
+    }) |>
+      debounce(1000) # 1000ms debounce
 
     period_filtered_data <- reactive({
       period_data <- debounced_data() |>
-        mutate(eventDate = as.Date(eventDate),
-               eventMonth = lubridate::month(eventDate))
+        mutate(
+          eventDate = as.Date(eventDate),
+          eventMonth = lubridate::month(eventDate)
+        )
 
-      switch(input$heatmap_periods,
-             'Spring' = {period_data |> filter(eventMonth %in% c(9, 10, 11))},
-             'Summer' = {period_data |> filter(eventMonth %in% c(12, 1, 2))},
-             'Autumn' = {period_data |> filter(eventMonth %in% c(3, 4, 5))},
-             'Winter' = {period_data |> filter(eventMonth %in% c(6, 7, 8))})
-    }) |> bindCache(input$heatmap_periods, debounced_data())
+      switch(
+        input$heatmap_periods,
+        'Spring' = {
+          period_data |> filter(eventMonth %in% c(9, 10, 11))
+        },
+        'Summer' = {
+          period_data |> filter(eventMonth %in% c(12, 1, 2))
+        },
+        'Autumn' = {
+          period_data |> filter(eventMonth %in% c(3, 4, 5))
+        },
+        'Winter' = {
+          period_data |> filter(eventMonth %in% c(6, 7, 8))
+        }
+      )
+    }) |>
+      bindCache(input$heatmap_periods, debounced_data())
 
     # Default leaflet.extras heatmap colors
     heatmap_colors <- c("#0000ff", "#00ffff", "#00ff00", "#ffff00", "#ff0000")
@@ -51,7 +65,7 @@ heatmapModuleServer <- function(id, filtered_data, taxa_level) {
       req(filtered_data(), taxa_level())
       name <- get_taxa_name(filtered_data(), taxa_level())
       name
-     })
+    })
 
     output$heatmap_legend <- renderUI({
       current_title <- legend_title()
@@ -60,9 +74,10 @@ heatmapModuleServer <- function(id, filtered_data, taxa_level) {
         # Grey background to legend
         style = "display: inline-block; padding: 5px 6px; background: #e0e0e0; background: rgba(224,224,224,0.8);
             box-shadow: 0 0 10px rgba(0,0,0,0.2); border-radius: 5px;",
-        tags$div(style = "font-weight: bold; font-size: 0.8em; margin-bottom: 2px;",
-                 paste0("Occurrence density - ", current_title)
-                 ),
+        tags$div(
+          style = "font-weight: bold; font-size: 0.8em; margin-bottom: 2px;",
+          paste0("Occurrence density - ", current_title)
+        ),
         tags$div(
           style = "display: flex; align-items: center;",
           lapply(1:length(heatmap_colors), function(i) {
@@ -70,8 +85,10 @@ heatmapModuleServer <- function(id, filtered_data, taxa_level) {
             tags$div(
               style = "display: flex; align-items: center; margin-right: 10px;",
               tags$div(
-                style = sprintf("width: 10px; height: 10px; background: %s; margin-right: 1px;",
-                                rev(heatmap_colors)[i])
+                style = sprintf(
+                  "width: 10px; height: 10px; background: %s; margin-right: 1px;",
+                  rev(heatmap_colors)[i]
+                )
               ),
               tags$div(labels[i], style = "font-size: 0.6em;")
             )
@@ -92,9 +109,12 @@ heatmapModuleServer <- function(id, filtered_data, taxa_level) {
     })
 
     output$heatmap <- renderLeaflet({
-      base_map <- leaflet(options = leafletOptions(attributionControl=FALSE)) |>
+      base_map <- leaflet(
+        options = leafletOptions(attributionControl = FALSE)
+      ) |>
         addTiles() |>
-        addProviderTiles(providers$CartoDB.Positron) |>
+        # addProviderTiles(providers$CartoDB.Positron) |>
+        addProviderTiles(providers$OpenStreetMap.Mapnik) |>
         addFullscreenControl() |>
         setView(lng = DEFAULT_LONG, lat = DEFAULT_LAT, zoom = DEFAULT_ZOOM) |>
         addScaleBar(position = "bottomleft")
@@ -103,7 +123,8 @@ heatmapModuleServer <- function(id, filtered_data, taxa_level) {
         base_map <- base_map |>
           leaflet.extras::addHeatmap(
             data = period_filtered_data(),
-            lng = ~longitude, lat = ~latitude,
+            lng = ~longitude,
+            lat = ~latitude,
             intensity = 1,
             blur = 20,
             max = 0.7,
@@ -118,10 +139,14 @@ heatmapModuleServer <- function(id, filtered_data, taxa_level) {
     observeEvent(period_filtered_data(), {
       df <- period_filtered_data()
       if (nrow(df) == 0 && !first_load()) {
-        showNotification(HTML("No data available for current selection.<br>Try selecting a different period."),
-                         type = "warning",
-                         duration = 2,
-                         id = "heatmap_warning")
+        showNotification(
+          HTML(
+            "No data available for current selection.<br>Try selecting a different period."
+          ),
+          type = "warning",
+          duration = 2,
+          id = "heatmap_warning"
+        )
         leafletProxy("heatmap") |>
           clearGroup("heatmap_cols")
       } else {
@@ -131,7 +156,8 @@ heatmapModuleServer <- function(id, filtered_data, taxa_level) {
           clearGroup("heatmap_cols") |>
           leaflet.extras::addHeatmap(
             data = df,
-            lng = ~longitude, lat = ~latitude,
+            lng = ~longitude,
+            lat = ~latitude,
             intensity = 1,
             blur = 20,
             max = 0.7,
